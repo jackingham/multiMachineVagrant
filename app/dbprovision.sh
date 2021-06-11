4732155620f252a0b6d59ccefb16db11abdad460
jackingham <jackingham1998@gmail.com>
2021-06-11 13:57:06 +0100

    Added more config but mongo still not running properly

--- app/dbprovision.sh
+++ app/dbprovision.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-sudo apt-get update -y
-
-sudo apt-get upgrade -y
-
-curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-
-sudo apt update
-
-sudo apt install mongodb-org
-
-sudo systemctl start mongod.service
-
-sudo systemctl status mongod
-
-sudo systemctl enable mongod
+ 
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list 
+sudo apt-get update -y 
+sudo apt-get upgrade -y 
+sudo apt-get install -y mongodb-org 
+sudo rm /etc/mongod.conf 
+sudo ln -s /home/vagrant/app/mongod.conf /etc/mongod.conf
